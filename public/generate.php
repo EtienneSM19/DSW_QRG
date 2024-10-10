b6222ca54aaed2d42a12c31b40d68c0eefe77c75
Etienne Santana <etiennemedina@gmail.com>
2024-10-10 16:07:18 +0100

    QR Generator

---
+++ public/generate.php
@@ -0,0 +1,46 @@
+<?php
+require '../vendor/autoload.php';
+
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\Label\LabelAlignment;
+use Endroid\QrCode\Label\Font\NotoSans;
+use Endroid\QrCode\Label\Font\OpenSans;
+use Endroid\QrCode\RoundBlockSizeMode;
+use Endroid\QrCode\Writer\PngWriter;
+
+$data = $_POST['data'];
+$label = $_POST['label'];
+
+$result = Builder::create()
+    ->writer(new PngWriter())
+    ->writerOptions([])
+    ->data($data)
+    ->encoding(new Encoding('UTF-8'))
+    ->errorCorrectionLevel(ErrorCorrectionLevel::High)
+    ->size(300)
+    ->margin(10)
+    ->roundBlockSizeMode(RoundBlockSizeMode::Margin)
+    ->labelText($label)
+    ->labelFont(new OpenSans(10))
+    ->labelAlignment(LabelAlignment::Center)
+    ->validateResult(false)
+    ->build();
+
+    $result->saveToFile(__DIR__.'/qrcode.png');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>QR Creator</title>
+</head>
+<body>
+  <h1>Código Qr Generado</h1>
+  <p>
+    <img src="<?= $result->getDataUri() ?>" alt="codigo QR generado">
+  </p>
+</body>
+</html>    
---
+++ public/prueba.php
@@ -0,0 +1,45 @@
+<?php
+require '../vendor/autoload.php';
+
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\Label\LabelAlignment;
+use Endroid\QrCode\Label\Font\NotoSans;
+use Endroid\QrCode\Label\Font\OpenSans;
+use Endroid\QrCode\RoundBlockSizeMode;
+use Endroid\QrCode\Writer\PngWriter;
+
+$data = $_POST['data'];
+$label = $_POST['label'];
+
+$result = Builder::create()
+    ->writer(new PngWriter())
+    ->writerOptions([])
+    ->data($data)
+    ->encoding(new Encoding('UTF-8'))
+    ->errorCorrectionLevel(ErrorCorrectionLevel::High)
+    ->size(300)
+    ->margin(10)
+    ->roundBlockSizeMode(RoundBlockSizeMode::Margin)
+    ->labelText($label)
+    ->labelFont(new OpenSans(20))
+    ->labelAlignment(LabelAlignment::Center)
+    ->validateResult(false)
+    ->build();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>QR Creator</title>
+</head>
+<body>
+  <h1>Código Qr Generado</h1>
+  <p>El texto: </p>
+  <p>
+    <img src="<?= $result->getDataUri() ?>" alt="codigo QR generado">
+  </p>
+</body>
+</html>    
